8ac5e106bef4bbf0d9d5fa2f74d81ee54c61888f
Aborn Jiang <loveaborn@foxmail.com>
2016-06-19 08:59:20 +0800

    add author info

--- pelpa-mode.el
+++ pelpa-mode.el
@@ -1,9 +1,24 @@
 ;;; pelpa-mode.el -- popkit elpa monitor mode-line
 
-;; Copyright 2016 Aborn Jiang
+;; Copyright (C) 2016 Aborn Jiang
 
 ;; Author: Aborn Jiang <aborn.jiang@gmail.com>
 ;; Version: 1.0
+;; Keywords: popkit, elpa, monitor
+;; Homepage: https://github.com/popkit/pelpa-mode
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
